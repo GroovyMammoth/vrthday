f6c895a71294e46d182f1686f69b9b5d09c25af4
Fidel Cano Renteria <groovymammoth@gmail.com>
2018-10-15 23:50:06 -0400

    Adding Cake Card

--- editables/load_texts.js
+++ editables/load_texts.js
@@ -1,12 +1,12 @@
 var TemplateHTML1;
 var TemplateHTML2;
+var TemplateHTML3;
 
 function preload(){
   TemplateHTML1 = loadStrings("editables/paradise.txt");
   TemplateHTML2 = loadStrings("editables/museum.txt");
+  TemplateHTML3 = loadStrings("editables/cake.txt");
 }
 
 function setup(){
-  console.log(TemplateHTML1);
-  console.log(TemplateHTML2);
 }
